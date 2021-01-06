b46b14055f7938e66bec29f41d11fadd62d90d24
Helio Dourado <heliohdd@yahoo.com.br>
2021-01-06 08:47:30 -0300

    Project created

---
+++ backend/hdsdeliver/src/main/java/com/hdd/hdsdeliver/HdsdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.hdd.hdsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdsdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdsdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/hdsdeliver/src/test/java/com/hdd/hdsdeliver/HdsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdd.hdsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

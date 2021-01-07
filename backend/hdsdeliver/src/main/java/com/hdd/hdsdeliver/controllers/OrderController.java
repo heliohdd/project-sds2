3cc435c086784329072d41b26f137ad665436f2a
Helio Dourado <heliohdd@yahoo.com.br>
2021-01-07 15:28:09 -0300

    PUT /orders/{id}/delivered

--- backend/hdsdeliver/src/main/java/com/hdd/hdsdeliver/controllers/OrderController.java
+++ backend/hdsdeliver/src/main/java/com/hdd/hdsdeliver/controllers/OrderController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,4 +37,11 @@ public class OrderController {
 				.buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
 	}
+	
+	@PutMapping("/{id}/delivered")
+	public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id) {
+		OrderDTO dto = orderService.setDelivered(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
 }
--- backend/hdsdeliver/src/main/java/com/hdd/hdsdeliver/services/OrderService.java
+++ backend/hdsdeliver/src/main/java/com/hdd/hdsdeliver/services/OrderService.java
@@ -42,4 +42,12 @@ public class OrderService {
 		order = orderRepository.save(order);
 		return new OrderDTO(order);
 	}
+
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = orderRepository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = orderRepository.save(order);
+		return new OrderDTO(order);
+	}
 }

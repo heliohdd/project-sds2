ba08cb6b86c22c20549f7ebfb74c7e0227b113aa
Helio Dourado <heliohdd@yahoo.com.br>
2021-01-07 15:09:21 -0300

    POST /orders

--- backend/hdsdeliver/src/main/java/com/hdd/hdsdeliver/controllers/OrderController.java
+++ backend/hdsdeliver/src/main/java/com/hdd/hdsdeliver/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.hdd.hdsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.hdd.hdsdeliver.dto.OrderDTO;
 import com.hdd.hdsdeliver.services.OrderService;
@@ -22,5 +26,13 @@ public class OrderController {
 	public ResponseEntity<List<OrderDTO>> findAll() {
 		List<OrderDTO> list = orderService.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+		dto = orderService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- backend/hdsdeliver/src/main/java/com/hdd/hdsdeliver/services/OrderService.java
+++ backend/hdsdeliver/src/main/java/com/hdd/hdsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.hdd.hdsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.hdd.hdsdeliver.dto.OrderDTO;
+import com.hdd.hdsdeliver.dto.ProductDTO;
 import com.hdd.hdsdeliver.entities.Order;
+import com.hdd.hdsdeliver.entities.OrderStatus;
+import com.hdd.hdsdeliver.entities.Product;
 import com.hdd.hdsdeliver.repositories.OrderRepository;
+import com.hdd.hdsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,9 +22,24 @@ public class OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private ProductRepository productRepository;
+
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
 		List<Order> list = orderRepository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = orderRepository.save(order);
+		return new OrderDTO(order);
 	}
 }

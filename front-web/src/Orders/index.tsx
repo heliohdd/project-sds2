38b865c9d46e261d4e774eb37923612724feed2d
Helio Dourado <heliohdd@yahoo.com.br>
2021-01-12 17:38:33 -0300

    Add routes and StepsHeader

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+import StepsHeader from './StepsHeader';
+
+function Orders() {
+    return(
+        <div>
+            <StepsHeader />
+        </div>  
+    )   
+}
+
+export default Orders;
